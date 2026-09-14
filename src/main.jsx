e33fdc9847be8d561fd6fbdd2864f203afb767a2
Ammar Hisyam <ammarhisyam151@gmail.com>
2026-09-14 09:04:39 +0700

    refactor: centralize design system migration layers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import './styles/globals.css';
-import './styles.css';
-import './fixes.css';
-import './design-system.css';
+import './styles/legacy.css';
 import './baseline-ui.css';
+import './design-system.css';
 import './kanban-convergence.css';
 import { App } from './App';
 import { ErrorPanel } from './components/common/ErrorPanel';
